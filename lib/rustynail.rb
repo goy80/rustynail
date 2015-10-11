f666d24492ac31e0cb5a6322964e045e74352bb8
goy <hyper.go.80@gmail.com>
2015-10-12 01:55:49 +0900

    dev

--- lib/rustynail.rb
+++ lib/rustynail.rb
@@ -2,12 +2,19 @@ require "rustynail/version"
 
 module Rustynail
 
-  @facet_columns = []
-  @keyword_target_columns = []
-  @table_name = "table"
+  @@facet_columns = []
+  @@keyword_target_columns = []
+  @@table_name = "my_table"
 
   def aaa
     p "aaaaAAA"
+  end
+
+  def self.extended( klass )
+    p "extending..."
+
+
+
   end
 
   def facet_options opt={}
@@ -20,7 +27,7 @@ module Rustynail
 
     # filterオプションの構築
     filter_conds = []
-    @facet_columns.each do | column |
+    @@facet_columns.each do | column |
       if filter.key? key
         filter_conds << "#{ column } == #{ filter[ column ] }"
       end
@@ -31,24 +38,24 @@ module Rustynail
     # --match_columns 全文検索対象カラム
     # --filter 絞込み条件
     #
-    sql = %!SELECT mroonga_command("select #{ @table_name } \
+    sql = %!SELECT mroonga_command("select #{ @@table_name } \
             --limit 0 \
-            --match_columns '#{ @keyword_target_columns.join("||") }' \
+            --match_columns '#{ @@keyword_target_columns.join("||") }' \
             --query '#{ filter[ :keyword ] }' #{filter_option} \
-            --drilldown '#{ @facet_columns.join(",") }' \
+            --drilldown '#{ @@facet_columns.join(",") }' \
             --drilldown_sortby '-_nsubrecs, _key' \
             --drilldown_limit -1 \
             ") AS facet_options !
 
 
-
+    Rails.logger.debug "sql=#{sql}"
     dum = connection.select sql
     res = JSON.parse( dum.first["facet_options"] )
     res.delete_at( 0 )
 
     ret = {}
 
-    @facet_columns.each_with_index do | column, idx |
+    @@facet_columns.each_with_index do | column, idx |
       2.times do
         res[ idx ].delete_at 0
       end
