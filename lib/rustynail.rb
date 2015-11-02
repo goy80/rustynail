55ba18e423e83ad8b35d18a09a71116a1caeaad8
goy <hyper.go.80@gmail.com>
2015-11-03 00:46:40 +0900

    いったんできた。

--- lib/rustynail.rb
+++ lib/rustynail.rb
@@ -21,56 +21,65 @@ module Rustynail
   end
 
 
-  def facet_options opt = {}
+  def facet_options opt = {}, filter = {}
+    begin
+      filter ||= {}
 
-    if @@keyword_target_columns.blank?
-      raise "keyword-target-columns not specified."
-    end
-
-    opt = {} if opt.nil?
-    filter = {}
-    opt.each do | key, value |
-      filter[ key.to_sym ] = value
-    end
-
-    # filterオプションの構築
-    filter_conds = []
-    @@facet_columns.each do | column |
-      if filter.key? column
-        filter_conds << "#{ column } == #{ filter[ column ] }"
+      if @@keyword_target_columns.blank?
+        raise "keyword-target-columns not specified."
       end
-    end
-    filter_option = filter_conds.length > 0 ? %!--filter '#{ filter_conds.join(" && ") }'! : ""
 
-    #
-    # --match_columns 全文検索対象カラム
-    # --filter 絞込み条件
-    #
-    sql = %!SELECT mroonga_command("select #{ @@table_name } \
-            --limit 0 \
-            --match_columns '#{ @@keyword_target_columns.join("||") }' \
-            --query '#{ filter[ :keyword ] }' #{filter_option} \
-            --drilldown '#{ @@facet_columns.join(",") }' \
-            --drilldown_sortby '-_nsubrecs, _key' \
-            --drilldown_limit -1 \
-            ") AS facet_options !
-    Rails.logger.debug "sql=#{sql}"
-
-    dum = connection.select sql
-    res = JSON.parse( dum.first["facet_options"] )
-    res.delete_at( 0 )
-
-    ret = {}
-    @@facet_columns.each_with_index do | column, idx |
-      2.times do
-        res[ idx ].delete_at 0
+      opt = {} if opt.nil?
+      opt.each do | key, value |
+        filter[ key.to_sym ] = value
       end
-      ret[ column.to_s ] = Hash[ *res[ idx ].flatten ]
+
+      # filterオプションの構築
+      filter_conds = []
+      @@facet_columns.each do | column |
+        if filter.key? column
+          value = filter[ column ]
+          unless self.columns.find{ |col| col.name == column.to_s }.number?
+            value = %!\""#{ value }\""!
+          end
+
+          filter_conds << "#{ column } == #{ value }"
+        end
+      end
+      filter_option = filter_conds.length > 0 ? %!--filter '#{ filter_conds.join(" && ") }'! : ""
+
+      #
+      # --match_columns 全文検索対象カラム
+      # --filter 絞込み条件
+      #
+      sql = %!SELECT mroonga_command("select #{ @@table_name } \
+              --limit 0 \
+              --match_columns '#{ @@keyword_target_columns.join("||") }' \
+              --query '#{ filter[ :keyword ] }' #{filter_option} \
+              --drilldown '#{ @@facet_columns.join(",") }' \
+              --drilldown_sortby '-_nsubrecs, _key' \
+              --drilldown_limit -1 \
+              ") AS facet_options !
+      Rails.logger.debug "sql=#{sql}"
+
+      dum = connection.select sql
+      res = JSON.parse( dum.first["facet_options"] )
+      res.delete_at( 0 )
+
+      ret = {}
+      @@facet_columns.each_with_index do | column, idx |
+        2.times do
+          res[ idx ].delete_at 0
+        end
+        ret[ column.to_s ] = Hash[ *res[ idx ].flatten ]
+      end
+
+      Rails.logger.debug "facet_options: #{ ret.inspect }"
+
+      ret
+    rescue => ex
+      Rails.logger.error "exception: message = #{ ex.message }. backtrace is bellow.\n #{ ex.backtrace.join("\n") }"
     end
-
-    Rails.logger.debug "facet_options: #{ ret.inspect }"
-
-    ret
 
   end
 
