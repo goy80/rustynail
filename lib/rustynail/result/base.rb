e7eed7da896b988177ec52abfbc8ddbbf1924eb5
goy <hyper.go.80@gmail.com>
2015-12-22 14:33:41 +0900

    modify ActionViewExtension.render_facet_optins opt[ :facet_option ]を引数で渡さずにresultから取得するように修正。

--- lib/rustynail/result/base.rb
+++ lib/rustynail/result/base.rb
@@ -14,16 +14,16 @@ module Rustynail::Result
     attr :list, :facet_options, :direction, :filter
 
     #
+    # @param [ Class ] include_class RustynailをMix-inしたクラス。
     # @param [ Hash ] opts
     # @option opts [ Hash ] :filter 選択された検索条件。
     # @option opts [ ActiveRecord::Relation ] :list 検索結果。
-    # @option opts [ Result::Options ] :facet_options: 検索結果の絞込みに使えるファセット検索選択肢。
     # @option opts [ Result::Direction ] :direction 検索結果のソート順オブジェクト。
     #
-    def initialize( opts={} )
+    def initialize( include_class, opts={} )
+      @include_class = include_class
       @filter = opts[ :filter ] || {}
       @list = opts[ :list ].presence || []
-      @facet_options = opts[ :facet_options ].presence || Options.new( {} )
       @direction = opts[ :direction ].presence || Direction.new( "" )
     end
 
@@ -35,7 +35,16 @@ module Rustynail::Result
     #
     #
     def selected_option?( column, option )
-      @filter[ column.to_sym ] == option
+      @filter[ column.to_s ] == option.to_s
+    end
+
+    #
+    # 検索結果の絞込みに使用可能なファセット検索選択肢
+    #
+    # @return [Result::Options]
+    #
+    def facet_options
+      @include_class.facet_options @filter
     end
 
   end
